9d1bf9777f27068234f3a39e2264d716af0d840e
Thomas Fenaroli <tfenaroli@icloud.com>
2023-04-11 22:10:37 -0400

    starter pack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let count = 0;
+
+$('#main').text(`You've been on this page for ${count} seconds.`);
+
+const updateTime = () => {
+  count += 1;
+  $('#main').text(`You've been on this page for ${count} seconds.`);
+};
+
+setInterval(updateTime, 1000);
